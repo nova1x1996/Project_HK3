7d86183ddf3bb6baea76407b2e01ef651919882e
Nguyen-Hoang-Bao-An <nguyenhoangbaoan12a13@gmail.com>
2023-02-11 20:14:08 +0700

    thêm các phần trong admin

---
+++ Project/Areas/Admin/Controllers/MainAdminController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MainAdminController : Controller
+    {
+        // GET: MainAdminController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MainAdminController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MainAdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MainAdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MainAdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MainAdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MainAdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MainAdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -21,7 +21,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=MainAdmin}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
