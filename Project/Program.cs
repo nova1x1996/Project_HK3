598f052e59a4b836ae0a3f03b0a507a05432b8bd
ThanhPhong <maithanhphong1812@gmail.com>
2023-02-13 15:51:32 +0700

    thêm database của Phong

--- Project/Program.cs
+++ Project/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 builder.Services.AddDbContext<DatabaseContext>(options => 
 
-options.UseSqlServer(builder.Configuration.GetConnectionString("WebDatabaseAn")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("WebDatabasePhong")));
 builder.Services.AddSession();
 var app = builder.Build();
 
